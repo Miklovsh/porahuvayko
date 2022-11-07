5439d6cf6222230076680060f04222d4ddb141a4
Miklovsh <miklovsh.vik@gmail.com>
2022-11-07 17:31:35 +0200

    Added logic for the first group.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React, { useState } from 'react';
+import React, { createRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+
+  let firstGroupInput = React.createRef();
+  const [firstGroupInputState, setFirstGroupInputState] = useState('');
 
   const [toggleState, setToggleState] = useState(1);
+  const [socialContributionState, setSocialContributionState] = useState('');
+  const [singleTaxState, setSingleTaxState] = useState('');
+  const [totalTaxAmountState, setTotalTaxAmount] = useState('');
+  const [amountReceivedState, setAmountReceived] = useState('');
+
 
   function toggleButton(index) {
     setToggleState(index);
+  }
+
+  function changeFirstGroupInput() {
+    setFirstGroupInputState(firstGroupInput.current.value);
+  }
+
+  function validateInput(e) {
+    let theEvent = e || window.event;
+    let key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
+    let regex = /[0-9]|\./;
+    if (!regex.test(key)) {
+      theEvent.returnValue = false;
+      if (theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
+
+
+  function countFirstGroup() {
+    if (firstGroupInputState.length > 0 && firstGroupInputState.length < 11) {
+      setSocialContributionState(`${props.socialContribution} гривень`);
+      setSingleTaxState(`${props.singleTax} гривень`);
+      setTotalTaxAmount(`${props.socialContribution + props.singleTax} гривень`);
+      setAmountReceived(`${firstGroupInput.current.value - props.socialContribution - props.singleTax} гривень`);
+    } else {
+      alert('Поле "Сума для оподаткування:" не може бути пустим або містити більше 10 символів!');
+    }
   }
 
   return (
@@ -34,14 +69,24 @@ function App() {
           <div className={toggleState === 1 ? "main__content main__content-active" : "main__content"}>
             <h3 className='main__content-title'>Калькулятор податків для ФОП 1 група</h3>
             <p className='main__content-description'>Єдиний соціальний внесок - 1 474 гривень(22% від мінімальної зарплати). Єдиний податок - 248 гривень 10 копійок(10% від прожиткового мінімуму станом на 1 січня)</p>
-            <label className='main__content-label'>Сума для оподаткування:</label><input className='main__content-input' type="text" name='first-group' /><label className='main__content-value'>гривень</label>
+            <label className='main__content-label'>Сума для оподаткування:</label><input className='main__content-input' onKeyPress={validateInput} onChange={changeFirstGroupInput} ref={firstGroupInput} value={firstGroupInputState} type="number" name='first-group' /><label className='main__content-value'>гривень</label>
             <div className='main__content-box'>
               <p className='main__content-out'>Єдиний соціальний внесок:</p>
-              <p className='main__content-out'>Єдиний податок:</p>
-              <p className='main__content-out'>Загальна сума податку:</p>
-              <p className='main__content-out'>На руки отримаєте:</p>
-              <input className='main__content-btn' type="button" value="Порахувати" />
+              <p className='main__content-out'>{socialContributionState}</p>
             </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>Єдиний податок:</p>
+              <p className='main__content-out'>{singleTaxState}</p>
+            </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>Загальна сума податку:</p>
+              <p className='main__content-out red'>{totalTaxAmountState}</p>
+            </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>На руки отримаєте:</p>
+              <p className='main__content-out green'>{amountReceivedState}</p>
+            </div>
+            <input className='main__content-btn' type="button" onClick={countFirstGroup} value="Порахувати" />
           </div>
           <div className={toggleState === 2 ? "main__content main__content-active" : "main__content"}>
             <h3 className='main__content-title'>Калькулятор податків для ФОП 2 група</h3>
@@ -49,11 +94,21 @@ function App() {
             <label className='main__content-label'>Сума для оподаткування:</label><input className='main__content-input' type="text" name='second-group' /><label className='main__content-value'>гривень</label>
             <div className='main__content-box'>
               <p className='main__content-out'>Єдиний соціальний внесок:</p>
-              <p className='main__content-out'>Єдиний податок:</p>
-              <p className='main__content-out'>Загальна сума податку:</p>
-              <p className='main__content-out'>На руки отримаєте:</p>
-              <input className='main__content-btn' type="button" value="Порахувати" />
+              <p className='main__content-out'></p>
             </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>Єдиний податок:</p>
+              <p className='main__content-out'></p>
+            </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>Загальна сума податку:</p>
+              <p className='main__content-out red'></p>
+            </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>На руки отримаєте:</p>
+              <p className='main__content-out green'></p>
+            </div>
+            <input className='main__content-btn' type="button" value="Порахувати" />
           </div>
           <div className={toggleState === 3 ? "main__content main__content-active" : "main__content"}>
             <h3 className='main__content-title'>Калькулятор податків для ФОП 3 група без ПДВ</h3>
@@ -61,11 +116,21 @@ function App() {
             <label className='main__content-label'>Сума для оподаткування:</label><input className='main__content-input' type="text" name='three-group' /><label className='main__content-value'>гривень</label>
             <div className='main__content-box'>
               <p className='main__content-out'>Єдиний соціальний внесок:</p>
-              <p className='main__content-out'>Єдиний податок:</p>
-              <p className='main__content-out'>Загальна сума податку:</p>
-              <p className='main__content-out'>На руки отримаєте:</p>
-              <input className='main__content-btn' type="button" value="Порахувати" />
+              <p className='main__content-out'></p>
             </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>Єдиний податок:</p>
+              <p className='main__content-out'></p>
+            </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>Загальна сума податку:</p>
+              <p className='main__content-out red'></p>
+            </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>На руки отримаєте:</p>
+              <p className='main__content-out green'></p>
+            </div>
+            <input className='main__content-btn' type="button" value="Порахувати" />
           </div>
           <div className={toggleState === 4 ? "main__content main__content-active" : "main__content"}>
             <h3 className='main__content-title'>Калькулятор податків для ФОПів які на загальній системі оподаткуванню</h3>
@@ -73,12 +138,25 @@ function App() {
             <label className='main__content-label'>Сума для оподаткування:</label><input className='main__content-input' type="text" name='general-group' /><label className='main__content-value'>гривень</label>
             <div className='main__content-box'>
               <p className='main__content-out'>Єдиний соціальний внесок:</p>
-              <p className='main__content-out'>Єдиний податок:</p>
-              <p className='main__content-out'>Військовий збір:</p>
-              <p className='main__content-out'>Загальна сума податку:</p>
-              <p className='main__content-out'>На руки отримаєте:</p>
-              <input className='main__content-btn' type="button" value="Порахувати" />
+              <p className='main__content-out'></p>
             </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>Єдиний податок:</p>
+              <p className='main__content-out'></p>
+            </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>Військовий збір:</p>
+              <p className='main__content-out'></p>
+            </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>Загальна сума податку:</p>
+              <p className='main__content-out red'></p>
+            </div>
+            <div className='main__content-box'>
+              <p className='main__content-out'>На руки отримаєте:</p>
+              <p className='main__content-out green'></p>
+            </div>
+            <input className='main__content-btn' type="button" value="Порахувати" />
           </div>
         </div>
       </main>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App socialContribution={1474} singleTax={248.10} />
   </React.StrictMode>
 );
 
